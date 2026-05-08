aa14da130738961f46732a23a61201ee41f9a85d
Kragline <armanaraqelyan1107@gmail.com>
2026-05-08 18:56:17 +0400

    added separate components

--- src/App.jsx
+++ src/App.jsx
@@ -1,120 +1,124 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import React from 'react'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import HomeCards from './components/HomeCards';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const names = ['Arman', 'Davo', 'Musho'];
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+      <Navbar />
+      <Hero />
+      <HomeCards />
+
+      <section className="bg-blue-50 px-4 py-10">
+        <div className="container-xl lg:container m-auto">
+          <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
+            Browse Jobs
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+
+            <div className="bg-white rounded-xl shadow-md relative">
+              <div className="p-4">
+                <div className="mb-6">
+                  <div className="text-gray-600 my-2">Full-Time</div>
+                  <h3 className="text-xl font-bold">Senior React Developer</h3>
+                </div>
+
+                <div className="mb-5">
+                We are seeking a talented Front-End Developer to join our team in Boston, MA. The ideal candidate will have strong skills in HTML, CSS, and JavaScript...
+                </div>
+
+                <h3 className="text-indigo-500 mb-2">$70 - $80K / Year</h3>
+
+                <div className="border border-gray-100 mb-5"></div>
+
+                <div className="flex flex-col lg:flex-row justify-between mb-4">
+                  <div className="text-orange-700 mb-3">
+                    <i className="fa-solid fa-location-dot text-lg"></i>
+                    Boston, MA
+                  </div>
+                  <a
+                    href="job.html"
+                    className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+                  >
+                  Read More
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="bg-white rounded-xl shadow-md relative">
+              <div className="p-4">
+                <div className="mb-6">
+                  <div className="text-gray-600 my-2">Remote</div>
+                  <h3 className="text-xl font-bold">Front-End Engineer (React)</h3>
+                </div>
+
+                <div className="mb-5">
+                Join our team as a Front-End Developer in sunny Miami, FL. We are looking for a motivated individual with a passion...
+                </div>
+
+                <h3 className="text-indigo-500 mb-2">$70K - $80K / Year</h3>
+
+                <div className="border border-gray-100 mb-5"></div>
+
+                <div className="flex flex-col lg:flex-row justify-between mb-4">
+                  <div className="text-orange-700 mb-3">
+                    <i className="fa-solid fa-location-dot text-lg"></i>
+                    Miami, FL
+                  </div>
+                  <a
+                    href="job.html"
+                    className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+                  >
+                  Read More
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="bg-white rounded-xl shadow-md relative">
+              <div className="p-4">
+                <div className="mb-6">
+                  <div className="text-gray-600 my-2">Remote</div>
+                  <h3 className="text-xl font-bold">React.js Developer</h3>
+                </div>
+
+                <div className="mb-5">
+                  Are you passionate about front-end development? Join our team in vibrant Brooklyn, NY, and work on exciting projects that make a difference...
+                </div>
+
+                <h3 className="text-indigo-500 mb-2">$70K - $80K / Year</h3>
+
+                <div className="border border-gray-100 mb-5"></div>
+
+                <div className="flex flex-col lg:flex-row justify-between mb-4">
+                  <div className="text-orange-700 mb-3">
+                    <i className="fa-solid fa-location-dot text-lg"></i>
+                    Brooklyn, NY
+                  </div>
+                  <a
+                    href="job.html"
+                    className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
+                  >
+                  Read More
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
+      </section>
+
+      <section className="m-auto max-w-lg my-10 px-6">
+        <a
+          href="jobs.html"
+          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+          >View All Jobs</a
         >
-          Count is {count}
-        </button>
       </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
     </>
   )
 }
